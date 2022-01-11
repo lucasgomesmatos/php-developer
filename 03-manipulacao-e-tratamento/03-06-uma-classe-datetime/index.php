64a39a1f2d03c3bfef0aa8a9fc77430fca532afc
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-01-10 23:31:25 -0300

    class date

--- 03-manipulacao-e-tratamento/03-06-uma-classe-datetime/index.php
+++ 03-manipulacao-e-tratamento/03-06-uma-classe-datetime/index.php
@@ -7,7 +7,14 @@ fullStackPHPClassName("03.06 - Uma classe DateTime");
  */
 fullStackPHPClassSession("A classe DateTime", __LINE__);
 
+define("DATE_BR", "d/m/Y H:i:s");
 
+$dateNow = new DateTime();
+
+var_dump(
+  $dateNow,
+    get_class_methods($dateNow)
+);
 /*
  * [ DateInterval ] http://php.net/manual/en/class.dateinterval.php
  * [ interval_spec ] http://php.net/manual/en/dateinterval.construct.php
