067f7c3506ec12e70781ce525b1ea6047cc5a7fa
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-01-11 22:45:13 -0300

    Manipulação de datetime

--- 03-manipulacao-e-tratamento/03-06-uma-classe-datetime/index.php
+++ 03-manipulacao-e-tratamento/03-06-uma-classe-datetime/index.php
@@ -10,16 +10,54 @@ fullStackPHPClassSession("A classe DateTime", __LINE__);
 define("DATE_BR", "d/m/Y H:i:s");
 
 $dateNow = new DateTime();
+$dateBirth = new DateTime("1999-01-02");
+$dateStatic = DateTime::createFromFormat("d/m/Y", "10/03/2020");
 
 var_dump(
   $dateNow,
-    get_class_methods($dateNow)
+    $dateBirth,
+    $dateStatic
 );
+
+var_dump(
+  $dateNow->format(DATE_BR),
+    $dateNow->format("d"),
+);
+
+echo "<p>Hoje é dia {$dateNow->format('d')} do mês {$dateNow->format('m')} de {$dateNow->format('Y')}</p>";
+
+$newTimeZone = new DateTimeZone("Pacific/Apia");
+$newDateTime = new DateTime("now", $newTimeZone);
+
+var_dump([
+    $newTimeZone,
+    $newDateTime,
+    $dateNow
+]);
 /*
  * [ DateInterval ] http://php.net/manual/en/class.dateinterval.php
  * [ interval_spec ] http://php.net/manual/en/dateinterval.construct.php
  */
 fullStackPHPClassSession("A classe DateInterval", __LINE__);
+
+$dateInterval = new DateInterval("P10Y2MT10M");
+
+var_dump([$dateInterval,]);
+
+
+$dateTime = new DateTime('now');
+$dateTime->add($dateInterval);
+$dateTime->sub($dateInterval);
+
+var_dump([$dateTime]);
+
+
+$birth = new DateTime(date("Y") . "-01-02");
+$dateNow = new DateTime("now");
+
+$diff = $dateNow->diff($birth);
+
+var_dump([$birth, $diff]);
 
 
 /**
