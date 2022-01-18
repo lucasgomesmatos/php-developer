7449272a3ea85e3fabef46582d5c26cfb2c9fa8e
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-01-17 22:53:56 -0300

    Gerenciamento de Arquivos com PHP

--- 03-manipulacao-e-tratamento/03-07-manipulacao-de-arquivos/index.php
+++ 03-manipulacao-e-tratamento/03-07-manipulacao-de-arquivos/index.php
@@ -9,10 +9,10 @@ fullStackPHPClassSession("verificação", __LINE__);
 
 $file = __DIR__ . "/file.txt";
 
-if (file_exists($file) && is_file($file)) {
+if(file_exists($file) && is_file($file)) {
     echo "<p>Existe!</p>";
 } else {
-    echo "<p>NÃO existe!</p>";
+    echo "<p>Não Existe!</p>";
 }
 
 
@@ -21,49 +21,53 @@ if (file_exists($file) && is_file($file)) {
  */
 fullStackPHPClassSession("leitura e gravação", __LINE__);
 
-if (!file_exists($file) || !is_file($file)) {
+if(!file_exists($file) || !is_file($file)) {
     $fileOpen = fopen($file, "w");
     fwrite($fileOpen, "Linha 01" . PHP_EOL);
     fwrite($fileOpen, "Linha 02" . PHP_EOL);
     fwrite($fileOpen, "Linha 03" . PHP_EOL);
-    fwrite($fileOpen, "Lorem Ipsum is simply dummy text of the printing and typesetting industry." . PHP_EOL);
+    fwrite($fileOpen, "Olá mundo, php foda demais" . PHP_EOL);
     fclose($fileOpen);
 } else {
     var_dump(
-        file($file),
-        pathinfo($file)
+      file($file),
+      pathinfo($file),
     );
 }
+
 
 echo "<p>", file($file)[3], "</p>";
 
 $open = fopen($file, "r");
-while (!feof($open)) {
-    echo "<p>" . fgets($open) . "</p>";
+while(!feof($open)) {
+    echo "<p>". fgets($open) . "</p>";
 }
 
 fclose($open);
-
 
 /*
  * [ get, put content ] file_get_contents | file_put_contents
  */
 fullStackPHPClassSession("get, put content", __LINE__);
 
-
 $getContents = __DIR__ . "/teste2.txt";
-if (file_exists($getContents) && is_file($getContents)) {
+if(file_exists($getContents) && is_file($getContents)) {
     echo file_get_contents($getContents);
 } else {
-    $data = "<article><h1>Robson</h1><p>CEO UpInside<br>cursos@upinside.com.br</p></article>";
+    $data = "<article><h1>Lucas</h1> <p>CEO da Alpha Conect<br> lucasgomesmatosdev@gmail.com</p></article>";
     file_put_contents($getContents, $data);
     echo file_get_contents($getContents);
 }
 
-
 //unlink($getContents);
 //unlink($file);
 
-if (file_exists(__DIR__ . "/teste3.txt") && is_file(__DIR__ . "/teste3.txt")) {
-    unlink(__DIR__ . "/teste3.txt");
+if(file_exists(__DIR__."/teste3.txt") && is_file(__DIR__."/teste3.txt")) {
+    unlink(__DIR__."/teste3.txt");
 }
+
+
+
+
+
+
